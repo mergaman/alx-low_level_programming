eb8950baa5be7aeba5e72f48d5110a67e65c1358
mergaman <merga.edu@gmail.com>
2023-02-16 04:36:33 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A preproccessor function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
