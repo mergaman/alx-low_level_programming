ea0ea7717777eabf9dd9af0c5db04f3568036217
mergaman <merga.edu@gmail.com>
2023-02-17 11:11:49 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: 0 on success
+ */
+int main (void)
+{
+
+int s = 97, e = 122;
+while (s <= e)
+{
+if (s != 'q' && s != 'e')
+putchar(s);
+s++
+}
+putchar ('\n');
+return (0);
+}
