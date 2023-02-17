b8406178a98c23a551d8ac83c91e812502609442
mergaman <merga.edu@gmail.com>
2023-02-17 14:01:39 -0800

    Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - block
+ * Description: digit
+ * Starting from 0,
+ * Return:0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(40 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
