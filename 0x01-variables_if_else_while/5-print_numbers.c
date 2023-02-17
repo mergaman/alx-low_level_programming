7f5496ceae6cd586e8245b99e1e8cf045086cc18
mergaman <merga.edu@gmail.com>
2023-02-17 11:20:53 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,14 @@
  *
  * Return: 0 on success
  */
-int main (void)
+int main(void)
 {
 
 int s = 97, e = 122;
 while (s <= e)
 {
 if (s != 'q' && s != 'e')
-putchar(s);
+putchar (s);
 s++
 }
 putchar ('\n');
