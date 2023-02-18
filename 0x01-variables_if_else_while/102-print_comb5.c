7f1ce931ff8c866a538c626b6822ec7b9e2327d6
mergaman <merga.edu@gmail.com>
2023-02-18 13:20:50 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,42 +7,30 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int num1 = 0, num2 = 1, num1max = 98, num2max = 99;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-
-	while (c <= 98)
+	while (num1 <= num1max)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = 0;
-		while (c2 <= 98)
+		num2 = num1 + 1;
+		while (num2 <= num2max)
 		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
+			putchar(num1 < 9 ? 0 + '0' : (num1 / 10) + '0');
+			putchar(num1 < 9 ? num1 + '0' : (num1 % 10) + '0');
+			putchar(' ');
+			putchar(num2 < 9 ? 0 + '0' : (num2 / 10) + '0');
+			putchar(num2 < 9 ? num2 + '0' : (num2 % 10) + '0');
 
-			if (c < c2)
+			if (num1 != num1max)
 			{
-				putchar(f_d);
-				putchar(l_d);
+				putchar(',');
 				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
-			c2++;
+
+			num2++;
 		}
-		c++;
+		num1++;
 	}
 	putchar('\n');
+
 	return (0);
 }
