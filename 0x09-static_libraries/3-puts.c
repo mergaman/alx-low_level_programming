62afdd7dd9d390c0d1a1f2c735d65ae5d00c9083
mergaman <merga.edu@gmail.com>
2023-03-10 08:36:52 -0800

    all tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @_puts: description
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return (0);
+}
