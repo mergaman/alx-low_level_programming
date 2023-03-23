389dd8371be7300d9900cd6e039c66c39db9d63b
mergaman <merga.edu@gmail.com>
2023-03-23 13:12:07 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
-	/**
-	 * int_index - returns index place if comparison = true, else -1
-	 * @array: array
-	 * @size: size of elements in array
-	 * @cmp: pointer to func of one of the 3 in main
-	 * Return: 0
-	 */
-	int int_index(int *array, int size, int (*cmp)(int))
-	{
+/**
+* int_index - returns index place if comparison = true, else -1
+* @array: array
+* @size: size of elements in array
+* @cmp: pointer to func of one of the 3 in main
+* Return: 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
 		int i;
 
 		if (array == NULL || size <= 0 || cmp == NULL)
@@ -18,5 +18,4 @@
 				return (i);
 		}
 		return (-1);
-	}
-
+}
