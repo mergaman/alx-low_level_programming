f897482a46d8f4b3bf8d876078b3f24a374e3071
mergaman <merga.edu@gmail.com>
2023-03-23 12:18:37 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,15 +1,15 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
-	/**
-	 * sum_them_all: Returns the sum of all its parameters.
-	 * @n: The number of parameters passed to the function.
-	 * @....A variable number of parameters to calculate the sum of.
-	 *
-	 * Return: if n == 0-0.
-	 * otherwise - the sum of all parameters.
-	 */
-	int sum_them_all(const unsigned int n, ...)
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of parameters passed to the function.
+ * @....A variable number of parameters to calculate the sum of.
+ *
+ * Return: if n == 0-0.
+ * otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum = 0;
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,11 @@
 #include "variadic_functions.h"
-	#include <stdarg.h>
-	#include <stdio.h>
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
-	void print_all(const char * const format, ...)
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
 	{
 		int i = 0;
 		char *str, *sep = "";
