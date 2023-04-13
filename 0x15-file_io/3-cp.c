d91ca5f9eb437134872ba32546a3e35b1f021604
mergaman <merga.edu@gmail.com>
2023-04-13 06:43:42 -0700

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,93 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - explain size of buffer which is 1024
+ * @file: store in alloacted area
+ *
+ * Return: A newly allocated buffer
+ *
+ */
+char *create_buffer(char *file)
+{
+char *buffer;
+buffer = malloc(sizeof(char) * 1024);
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO);
+exit(99);
+}
+return (buffer);
+}
+
+/**
+ * close_file - switchedd off explained file
+ * @fd: the explained file is to be shutdown
+ */
+void close_file(int fd)
+{
+int c;
+c = close(fd);
+if (c == -1)
+{
+dprintf(STDERR_FILENO);
+}
+}
+
+/**
+ * main - multiply the printed file for alx students
+ * @argc: category of file to be printed
+ * @argv: function of files to be explained later
+ *
+ * Return: success(0)
+ *
+ * Description: explain everything when customer is asks you
+ *
+ */
+int main(int argc, char *argv[])
+{
+int from, to, r, w;
+char *buffer;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO);
+exit(97)
+}
+
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+	if (from == -1 || r == -1)
+	{
+		dprintf(STDERR_FILENO);
+		free(buffer);
+		exit(98);
+	}
+
+	w = write(to, buffer, r);
+	if (to == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO);
+		free(buffer);
+		exit(99);
+	}
+
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_WRONLY | O_APPEND);
+} while (r > 0);
+
+free(buffer);
+close_file(from);
+close_file(to);
+
+return (0);
+}
+
